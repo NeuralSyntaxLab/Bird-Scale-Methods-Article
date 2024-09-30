7748ac6e33c9e3f052e5430dbf4cb899069a12fd
Yuval Bonneh <140716203+Yuvalb94@users.noreply.github.com>
2024-09-30 15:59:14 +0300

    Update #2
    addition of all current items:
    1. parts list
    2. scale design
    3. arduino code

---
+++ arduino_code/arduino_code_for_scale/arduino_code_for_scale.ino
@@ -0,0 +1,153 @@
+/***************************************************************************
+* THIS IS THE MAIN ARDUINO CODE FOR ACOUSTIC CHAMBERS LIGHT CYCLE CONTROL, AS WELL AS SENSOR(DHT+LIGHT) & WEIGHT DATA ACQUISITION
+
+* It has two functionalities -
+*   1. Turn the lights on/off, based on an input from the serial port.
+*       Data from the serial port arrives as an int, and so we set a
+*       threshold to decide whether the light goes on or off.
+*
+*   2. Read data from temp. and humidity sensors, as well as light and weight
+*     sensors, and print values to the serial port (via Serial.print()).
+*     This data will later be read by our Raspberry Pi device, whenever
+*     we're ready to consume the data.
+
+NOTE: PLEASE MAKE SURE THAT THE FOLLOWING LIBRARY IS INSTALLED IN ARDUINO IDE:
+SparkFun I2C Mux Arduino Library
+
+***************************************************************************/
+
+#include <Wire.h>
+#include <EEPROM.h> //Needed to record user settings
+#include <SparkFun_I2C_Mux_Arduino_Library.h>
+#include <SparkFun_Qwiic_Scale_NAU7802_Arduino_Library.h>
+
+#define DHT22_PIN 8
+QWIICMUX myMux;
+NAU7802 myScale;
+
+//EEPROM locations to store 20-byte variables
+#define LOCATION_CALIBRATION_FACTOR(port) (port * 20) // Float, requires 4 bytes, plus extra space.
+#define LOCATION_ZERO_OFFSET(port) (LOCATION_CALIBRATION_FACTOR(port) + 8) // //Must be more than 4 away from previous spot. Long, requires 4 bytes of EEPROM
+bool settingsDetected = false; //Used to prompt user to calibrate their scale
+
+const int numScales = 8; // Number of Qwiic Scale ports available
+int samplesToTake = 16; //Number of samples to average over when calculating weight using getWeight function. Default is 8
+
+int incomingByte = 0; // Variable that will contain user input
+
+unsigned long seconds = 1000L;
+unsigned long minutes = seconds * 1;
+unsigned long DelayRate = minutes;
+
+// Variables to track loop timings
+unsigned long loopStartTime;
+unsigned long totalLoopTime = 0;
+int loopCounter = 0;
+
+void setup() {
+  // pinMode(LIGHT_SWITCH_PIN, OUTPUT);
+  // pinMode(ldrPin, INPUT);
+  Wire.begin();
+
+  Serial.begin(9600);
+
+  // myScale setup and scale parameters acquisition
+  if (!myMux.begin()) {
+  Serial.println("Mux not detected. Make sure scale system is connected properly and try again. Swich to arduino_code_0 to monitor DHT data alone, or to arduino_code_2 to test and calibrate scale.");
+  delay(DelayRate);
+  }
+  else {
+  // Serial.println("Mux detected");
+  for (int i = 0; i < numScales; ++i) {
+    myMux.enablePort(i);
+    myScale.begin();
+
+    if (myScale.begin()) {
+
+      myScale.setSampleRate(NAU7802_SPS_320); //Increase to max sample rate
+      myScale.calibrateAFE(); //Re-cal analog front end when we change gain, sample rate, or channel 
+    }
+    myMux.disablePort(i);
+  } 
+  } 
+}
+
+void loop() {
+  loopStartTime = millis(); // Record start time for each loop
+
+  for (int i = 0; i < numScales; ++i) {
+    myMux.setPort(i); // Activate communication with active scale #[i], disable all other ports
+ 
+    if (myScale.available() == true) {
+      readSystemSettings(i);
+      float currentScaleReading = myScale.getWeight(false,samplesToTake);
+      if (!isnan(currentScaleReading)) {
+        Serial.print(currentScaleReading, 2);
+        Serial.print(";");   
+      } else {
+        Serial.print(0);
+        Serial.print(";");
+      } 
+    } 
+    else {
+      Serial.print(0);
+      Serial.print(";");
+    }
+  }
+  Serial.println("");
+  myMux.setPort(-1); // disable all ports
+
+  // delay(DelayRate); //wait 1 second until next data reading
+
+  // End of loop processing - calculate loop time
+  unsigned long loopEndTime = millis();
+  unsigned long loopTime = loopEndTime - loopStartTime;
+
+  totalLoopTime += loopTime; // Accumulate loop times
+  loopCounter++; // Increment loop counter
+
+  // // Check every 10 loops
+  // if (loopCounter % 10 == 0) {
+  //   unsigned long averageTime10 = totalLoopTime / 10;
+  //   Serial.print("Average loop time for last 10 loops: ");
+  //   Serial.print(averageTime10);
+  //   Serial.println(" ms");
+  //   totalLoopTime = 0; // Reset total time after 10 loops
+  //   delay(5000);
+  // }
+
+}
+
+//Scale functions for communicating with EEPROM(non-vlatile memory of arduino)
+
+//Reads the current system settings from EEPROM
+//If anything looks weird, reset setting to default value
+void readSystemSettings(int scalePort) //REQUIRED to read an already-calibrated system settings (zero offset and calibration factor)
+{
+  float settingCalibrationFactor; //Value used to convert the load cell reading to lbs or kg
+  long settingZeroOffset; //Zero value that is found when scale is tared
+
+  //Look up the calibration factor
+  EEPROM.get(LOCATION_CALIBRATION_FACTOR(scalePort), settingCalibrationFactor);
+  if (settingCalibrationFactor == 0xFFFFFFFF)
+  {
+    settingCalibrationFactor = 0; //Default to 0
+    EEPROM.put(LOCATION_CALIBRATION_FACTOR(scalePort), settingCalibrationFactor);
+  }
+
+  //Look up the zero tare point
+  EEPROM.get(LOCATION_ZERO_OFFSET(scalePort), settingZeroOffset);
+  if (settingZeroOffset == 0xFFFFFFFF)
+  {
+    settingZeroOffset = 1000L; //Default to 1000 so we don't get inf
+    EEPROM.put(LOCATION_ZERO_OFFSET(scalePort), settingZeroOffset);
+  }
+
+  //Pass these values to the library
+  myScale.setCalibrationFactor(settingCalibrationFactor);
+  myScale.setZeroOffset(settingZeroOffset);
+
+  settingsDetected = true; //Assume for the moment that there are good cal values
+  if (settingCalibrationFactor < 0.1 || settingZeroOffset == 1000)
+    settingsDetected = false; //Defaults detected. Prompt user to cal scale.
+}
